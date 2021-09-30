7a6a91107d26bd7e795cff3418eac93412c14328
Fedor <krishnaindh@hotmail.com>
2021-09-30 23:16:09 +0300

    new file code.py Прошли первый урок!

---
+++ Savchuk_Fedor_dz_1/code.py
@@ -0,0 +1,50 @@
+"""
+print(int(5.87))
+print(float(5.55))
+print(bool(1))
+print(bool(0))
+print(bool(''))
+print(bool('Я текст'))
+a = 5
+b = 8
+print(a + b)
+
+# Я комментарий
+
+"""
+
+# hello = None
+#
+# if not hello:
+#     print(hello)
+# elif hello != 'sdf':
+#     proint(hello)
+# else:
+#     print('stop')
+#
+# print('end')
+
+# a = 10
+# while a >= 0:
+#     print(a)
+#     a += -1
+#
+# while True:
+#     if a >= 15:
+#         break
+#     a += 1
+#
+# print('end')
+
+# for x in [10, 25, 3]:
+#     print(x)
+
+# for x in range(1, 6, 2):
+#     print(x)
+#
+# print('end')
+
+# print(len([10, 25, 3]))
+
+# for index, key in enumerate(['a', 'b', 'c'], start=1):
+#     print(f'{index} - {key}')
