9d116fb2aca4e8289538868dfc2e218fc748c8d1
Fedor <krishnaindh@hotmail.com>
2021-10-03 20:52:22 +0300

    update dz_1_2.py

--- dz_1/dz_1_2.py
+++ dz_1/dz_1_2.py
@@ -1,2 +1,38 @@
 # Задание второе
+number_list = []
+sum = 0
+x = 0
 
+for x in range (1, 1000, 2):
+    x = x ** 3
+    number_list.append(x)
+
+x = 0
+
+while x <= (len(number_list) - 1):
+    divisibility_check = number_list[x]
+    sum_this_digits = 0
+    while divisibility_check != 0:
+        sum_this_digits += divisibility_check % 10
+        divisibility_check = divisibility_check // 10
+        # print(sum_this_digits, divisibility_check)
+    if sum_this_digits % 7 == 0:
+        sum = sum + number_list[x]
+        # print(cube_sum, cube_list[i], i, sum_this_digits)
+    x += 1
+print(sum)
+
+
+
+
+# a = input('Введите число: ')
+#
+# suma = 0
+# mult = 1
+#
+# for digit in a:
+#     suma += int(digit)
+#     mult *= int(digit)
+#
+# print("Сумма:", suma)
+# print("Произведение:", mult)
